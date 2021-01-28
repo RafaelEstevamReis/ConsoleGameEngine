5fbec5f75d6c1d09c70fde029785f63ab675491c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-28 18:30:07 -0300

    Demo

--- ConsoleGameEngine/Program.cs
+++ ConsoleGameEngine/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using System.Drawing;
+using Simple.CGE;
+using Simple.CGE.Helpers;
+using Simple.CGE.Prefab.PrefabDrawable;
 
-namespace Simple.CGE.Tests
+int vSpeedSign = 1;
+int hSpeedSign = 1;
+
+CGEngine cg = new CGEngine();
+cg.OnSetup += (e, a) =>
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    cg.TargetDrawFps = 0;
+    cg.AddEntities(new UIText(new PointF(),
+                              true,
+                              "Hello World",
+                              update));
+};
+cg.Setup();
+cg.Run();
+
+void update(FrameData data, UIText text)
+{
+    text.Text = $"Hello World at {data.Engine.CurrentFPS:0000} fps";
+    text.MoveBy(new PointF(1 * hSpeedSign, 1 * vSpeedSign), data.LastFrameTime);
+
+    if (text.Position.X > data.DrawEngine.GameBorder.Width - text.Text.Length) hSpeedSign = -1;
+    if (text.Position.X < 0) hSpeedSign = 1;
+
+    if (text.Position.Y > data.DrawEngine.GameBorder.Height) vSpeedSign = -1;
+    if (text.Position.Y < 0) vSpeedSign = 1;
 }
